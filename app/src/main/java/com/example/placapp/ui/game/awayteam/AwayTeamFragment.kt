f956ecc6f0e8f8680a3c0e95c2786e1277b6e727
Logon Aluno <logonaluno@local.com>
2020-02-08 17:23:28 -0300

    Initial commit

---
+++ app/src/main/java/com/example/placapp/ui/game/awayteam/AwayTeamFragment.kt
@@ -0,0 +1,61 @@
+package com.example.placapp.ui.game.awayteam
+
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+
+import com.example.placapp.R
+import com.example.placapp.ui.extensions.setupClearButtonWithAction
+import kotlinx.android.synthetic.main.fragment_away_team.*
+
+
+
+class AwayTeamFragment : Fragment() {
+
+    interface AwayTeamSelectedListener{
+        fun onAwayTeam(awayName: String)
+    }
+
+    private var listener: AwayTeamSelectedListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is AwayTeamSelectedListener){
+            listener = context
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_away_team, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        inputAwayTeam.setupClearButtonWithAction()
+        btNextStep.setOnClickListener {
+            sendAwayTeamName()
+        }
+    }
+
+    private fun sendAwayTeamName() {
+        /*val intent = Intent("FILTER_AWAY_TEAM")
+        intent.putExtra("away_team", inputAwayTeam.text.toString())
+        LocalBroadcastManager.getInstance(requireContext()).sendBroadcast(intent)*/
+
+        this.listener?.onAwayTeam(inputAwayTeam.text.toString())
+    }
+
+
+
+
+}
